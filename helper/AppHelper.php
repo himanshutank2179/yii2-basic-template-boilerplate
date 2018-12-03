80537aba90be5f28d94d86ec132395a077d2fbff
himanshu <himanshutank1111@gmail.com>
2018-12-03 15:40:37 +0530

    ...

--- helper/AppHelper.php
+++ helper/AppHelper.php
@@ -9,114 +9,114 @@ use app\models\TicketValues;
 class AppHelper
 {
 
-	static public function getLocations()
-	{
-		$locations = Location::find()->all();
-        return ArrayHelper::map($locations, 'location_id', 'location_name');
-	}
-
-	static public function getHours()
+    static public function getLocations()
     {
-        $hours =[
-        				'0' => '0',
-                        '1' => '1',
-                        '2' => '2',
-                        '3' => '3',
-                        '4' => '4',
-                        // '5' => '5',
-                        '6' => '6',
-                        // '7' => '7',
-                        '8' => '8',
-                        // '9' => '9',
-                        // '10' => '10',
-                        // '11' => '11',
-                        '12' => '12',
-                        // '13' => '13',
-                        // '14' => '14',
-                        // '15' => '15',
-                        // '16' => '16',
-                        // '17' => '17',
-                        // '18' => '18',
-                        // '19' => '19',
-                        // '20' => '20',
-                        // '21' => '21',
-                        // '22' => '22',
-                        // '23' => '23',
-                        // '24' => '24',
-                    ];
+        $locations = Location::find()->all();
+        return ArrayHelper::map($locations, 'location_id', 'location_name');
+    }
+
+    static public function getHours()
+    {
+        $hours = [
+            '0' => '0',
+            '1' => '1',
+            '2' => '2',
+            '3' => '3',
+            '4' => '4',
+            // '5' => '5',
+            '6' => '6',
+            // '7' => '7',
+            '8' => '8',
+            // '9' => '9',
+            // '10' => '10',
+            // '11' => '11',
+            '12' => '12',
+            // '13' => '13',
+            // '14' => '14',
+            // '15' => '15',
+            // '16' => '16',
+            // '17' => '17',
+            // '18' => '18',
+            // '19' => '19',
+            // '20' => '20',
+            // '21' => '21',
+            // '22' => '22',
+            // '23' => '23',
+            // '24' => '24',
+        ];
         return $hours;
     }
 
     static public function getMinutes()
     {
-        $hours =[
-    				'0' => '0',
-                    // '1' => '1',
-                    // '2' => '2',
-                    // '3' => '3',
-                    // '4' => '4',
-                    '5' => '5',
-                    // '6' => '6',
-                    // '7' => '7',
-                    // '8' => '8',
-                    // '9' => '9',
-                    '10' => '10',
-                    // '11' => '11',
-                    // '12' => '12',
-                    // '13' => '13',
-                    // '14' => '14',
-                    '15' => '15',
-                    // '16' => '16',
-                    // '17' => '17',
-                    // '18' => '18',
-                    // '19' => '19',
-                    '20' => '20',
-                    // '21' => '21',
-                    // '22' => '22',
-                    // '23' => '23',
-                    // '24' => '24',
-                    '25' => '25',
-                    // '26' => '26',
-                    // '27' => '27',
-                    // '28' => '28',
-                    // '29' => '29',
-                    '30' => '30',
-                    // '31' => '31',
-                    // '32' => '32',
-                    // '33' => '33',
-                    // '34' => '34',
-                    // '35' => '35',
-                    // '36' => '36',
-                    // '37' => '37',
-                    // '38' => '38',
-                    // '39' => '39',
-                    // '40' => '40',
-                    // '41' => '41',
-                    // '42' => '42',
-                    // '43' => '43',
-                    // '44' => '44',
-                    // '45' => '45',
-                    // '46' => '46',
-                    // '47' => '47',
-                    // '48' => '48',
-                    // '49' => '49',
-                    // '50' => '50',
-                    // '51' => '51',
-                    // '52' => '52',
-                    // '53' => '53',
-                    // '54' => '54',
-                    // '55' => '55',
-                    // '56' => '56',
-                    // '57' => '57',
-                    // '58' => '58',
-                    // '59' => '59',
-                ];
+        $hours = [
+            '0' => '0',
+            // '1' => '1',
+            // '2' => '2',
+            // '3' => '3',
+            // '4' => '4',
+            '5' => '5',
+            // '6' => '6',
+            // '7' => '7',
+            // '8' => '8',
+            // '9' => '9',
+            '10' => '10',
+            // '11' => '11',
+            // '12' => '12',
+            // '13' => '13',
+            // '14' => '14',
+            '15' => '15',
+            // '16' => '16',
+            // '17' => '17',
+            // '18' => '18',
+            // '19' => '19',
+            '20' => '20',
+            // '21' => '21',
+            // '22' => '22',
+            // '23' => '23',
+            // '24' => '24',
+            '25' => '25',
+            // '26' => '26',
+            // '27' => '27',
+            // '28' => '28',
+            // '29' => '29',
+            '30' => '30',
+            // '31' => '31',
+            // '32' => '32',
+            // '33' => '33',
+            // '34' => '34',
+            // '35' => '35',
+            // '36' => '36',
+            // '37' => '37',
+            // '38' => '38',
+            // '39' => '39',
+            // '40' => '40',
+            // '41' => '41',
+            // '42' => '42',
+            // '43' => '43',
+            // '44' => '44',
+            // '45' => '45',
+            // '46' => '46',
+            // '47' => '47',
+            // '48' => '48',
+            // '49' => '49',
+            // '50' => '50',
+            // '51' => '51',
+            // '52' => '52',
+            // '53' => '53',
+            // '54' => '54',
+            // '55' => '55',
+            // '56' => '56',
+            // '57' => '57',
+            // '58' => '58',
+            // '59' => '59',
+        ];
         return $hours;
     }
 
     static public function getTime()
     {
-        $time =[
+        $time = [
             '00:00:00' => '12:00 AM',
             '00:30:00' => '12.30 AM',
             '01:00:00' => '01:00 AM',
@@ -168,5 +168,55 @@ class AppHelper
         ];
         return $time;
     }
+
+    static public function SendNotification($message)
+    {
+        //$reg_ids = Devices::find()->select(['device_hardware_id'])->asArray()->all();
+        $regIdsArray = [];
+        $reg_ids = Devices::find()->select(['notification_id'])->all();
+        foreach ($reg_ids as $key => $value) {
+            # code...
+            array_push($regIdsArray,$value->notification_id);
+        }
+
+
+
+        /* echo '<pre>';
+         print_r($regIdsArray); exit();*/
+        $API_KEY = 'AAAAE1juTr0:APA91bH3oD9WmlBXrpTq_S0pTsjg0own9Va8rMpicPD0J3QN4YC_ucfA1lMYNlVQNLz_tVU9oE9KpYV-vod7uIiVsv0C__a6VRNBPMvX2tkGLR9EKK9Tj7fAD76gLVvapz67KBQNsY0V';
+
+        $path_to_firebase_cm = 'https://fcm.googleapis.com/fcm/send';
+
+        $fields =  array(
+            'registration_ids' => $regIdsArray,
+            'notification' => array('title' => 'New Image update', 'body' => $message),
+            'data' =>  array('message' =>$message,
+                'click_action' => 'com.arastu.connectutility_Notification'),
+
+        );
+
+
+        $headers = array(
+            'Authorization:key=' . $API_KEY,
+            'Content-Type:application/json'
+        );
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $path_to_firebase_cm);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $result;
+
+    }
 }
+
 ?>
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -106,3 +106,29 @@
 
     <?php endforeach; ?>
 </div>
+
+
+<?php
+$this->registerJs('
+    toastr.options = {
+      "closeButton": false,
+      "debug": false,
+      "newestOnTop": false,
+      "progressBar": false,
+      "positionClass": "toast-bottom-left",
+      "preventDuplicates": false,
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+    }
+    
+    toastr.info("Info Message", "' . date('Y-m-d') . '");
+
+', \yii\web\View::POS_END);
+?>
